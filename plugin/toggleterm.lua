138a55d47dc64c841fb2ac9e2c3204d89b07b75f
will.ochowicz <will.ochowicz@genusplc.com>
2023-08-23 16:16:09 -0500

    Remove event sim code from toggleterm init

--- plugin/toggleterm.lua
+++ plugin/toggleterm.lua
@@ -22,7 +22,6 @@ require('toggleterm').setup{
 local Terminal = termTable.Terminal
 
 local _lazygit_buffer_id = 99
-local _event_sim_node_buffer_id = 100
 
 --Ensure lazygit always opens the repo of the current directory
 function _lazygit_toggle()
@@ -48,27 +47,10 @@ end
 
 vim.keymap.set("n", "<leader>g1", _lazygit_toggle, {noremap = true, silent = true})
 
-local event_sim = Terminal:new({
-    id = _event_sim_node_buffer_id,
-    dir = "~/Local_Repository/event_sim_node/",
-    cmd = "npm run start",
-    direction = "horizontal",
-    float_opts = {
-        border = "double"
-    },
-    start_in_insert = false
-})
-
-function _event_sim_toggle()
-    event_sim:toggle()
-end
-
 function _new_terminal()
     local term = Terminal:new { }
     term:toggle();
 end
-
-vim.keymap.set("n", "<leader>g2", _event_sim_toggle, {noremap = true, silent = true})
 
 vim.keymap.set("n", "<C-J>", _new_terminal, {noremap = true, silent = true})
 
