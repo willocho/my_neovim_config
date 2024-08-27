25b8b4eddeb8b9143af9a895cb5a0ef960362150
Will Ochowicz <wochowicz@findhelp.com>
2024-08-27 10:37:12 -0500

    Update config

--- lua/config.lua
+++ lua/config.lua
@@ -2,15 +2,16 @@ vim.g.mapleader = ","
 vim.g.maplocalleader = " "
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
   pattern = {"Justfile"},
-  callback = function(ev)
+  callback = function(_)
       vim.api.nvim_set_option_value('expandtab', false, {})
   end
 })
 
 vim.api.nvim_create_autocmd({"BufLeave", "BufWinLeave"}, {
   pattern = {"Justfile"},
-  callback = function(ev)
+  callback = function(_)
       vim.api.nvim_set_option_value('expandtab', true, {})
   end
 })
+
 require("config.lazy")
