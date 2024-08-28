7794fca221a0b366328ba18b526988589a484cde
Will Ochowicz <wochowicz@findhelp.com>
2024-08-28 10:00:00 -0500

    Fix telescope

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,36 +1,34 @@
 local actions = require('telescope.actions')
 local telescope = require('telescope')
 
-telescope.setup{ 
-    opt = {
-        pickers = {
-            find_files = {
-                find_command = {
-                    'fd',
-                    '--color=never',
-                    '-H',
-                }
+telescope.setup{
+    pickers = {
+        find_files = {
+            find_command = {
+                'fd',
+                '--color=never',
+                '-H',
+            }
+        }
+    },
+    defaults = {
+        mappings = {
+            i = {
+                ["<A-j>"] = function () actions.move_selection_next(vim.api.nvim_get_current_buf()) end,
+                ["<A-k>"] = function () actions.move_selection_previous(vim.api.nvim_get_current_buf()) end,
+                ["<A-h>"] = function () vim.api.nvim_cmd({ cmd = "normal", args = {"h"}}, {}) end,
+                ["<A-l>"] = function () vim.api.nvim_cmd({ cmd = "normal", args = {"l"}}, {}) end,
             }
         },
-        defaults = {
-            mappings = {
-                i = {
-                    ["<A-j>"] = function () actions.move_selection_next(vim.api.nvim_get_current_buf()) end,
-                    ["<A-k>"] = function () actions.move_selection_previous(vim.api.nvim_get_current_buf()) end,
-                    ["<A-h>"] = function () vim.api.nvim_cmd({ cmd = "normal", args = {"h"}}, {}) end,
-                    ["<A-l>"] = function () vim.api.nvim_cmd({ cmd = "normal", args = {"l"}}, {}) end,
-                }
-            },
-            vimgrep_arguments = {
-                'rg',
-                '--color=never',
-                '--no-heading',
-                '--with-filename',
-                '--line-number',
-                '--column',
-                '--smart-case',
-            },
-        }
+        vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+        },
     }
 }
 
