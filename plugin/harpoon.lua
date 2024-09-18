11242418cdd77da609c3c998239e60aa30cdbd48
Will Ochowicz <wochowicz@findhelp.com>
2024-09-18 09:20:58 -0500

    Update harpoon keymaps

--- plugin/harpoon.lua
+++ plugin/harpoon.lua
@@ -1,6 +1,7 @@
 local ui = require('harpoon.ui')
 local mark = require('harpoon.mark')
 
-vim.keymap.set('n', '<Leader>h', ui.toggle_quick_menu, {})
-vim.keymap.set('n', '<Leader>m', mark.add_file, {})
+vim.keymap.set('n', '<Leader>hh', ui.toggle_quick_menu, {})
+vim.keymap.set('n', '<Leader>hm', mark.add_file, {})
+vim.keymap.set('n', '<Leader>hf', mark.store_offset, {})
 
