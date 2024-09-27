c9089d1c2031015b6fd6d0a370310ae536f9ba88
Will Ochowicz <wochowicz@findhelp.com>
2024-09-27 13:15:39 -0500

    RipGrep across whole git repository

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -34,6 +34,7 @@ telescope.setup{
 
 vim.api.nvim_set_keymap('n', 'gp', ':Telescope projects<CR>', {})
 
+--Look at all files in the top level git directory
 vim.keymap.set('n', '<Leader>F',
 function ()
     local handle = io.popen('git rev-parse --show-toplevel')
@@ -53,6 +54,38 @@ function ()
                     'fd',
                     '--color=never',
                     '-H',
+                }
+            }
+        end
+    end
+end,
+{}
+)
+
+--Grep from the top level git directory
+vim.keymap.set('n', '<Leader>G',
+function ()
+    local handle = io.popen('git rev-parse --show-toplevel')
+    if handle ~= nil then
+        local result_path = handle:read('*a')
+        local result = {handle:close()}
+
+        if result[1] and 
+            (result[2] == "exit" or result[2] == nil) and
+            (result[3] == 0 or result[3] == nil) then
+            local builtin = require('telescope.builtin')
+            --remove trailing newline
+            result_path = result_path:gsub('\n', '')
+            builtin.live_grep{
+                cwd = result_path,
+                find_command = {
+                  'rg',
+                  '--color=never',
+                  '--no-heading',
+                  '--with-filename',
+                  '--line-number',
+                  '--column',
+                  '--smart-case',
                 }
             }
         end
